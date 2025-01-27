3154d1e8d00d1ecaf2ff2a6233c0f539a8e056dc
AriCamargos <arianacamargos16@gmail.com>
2025-01-26 23:05:15 -0300

    feature: main

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:api_marvel/module/api_marvel_module.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+void main() {
+  runApp(
+    ModularApp(
+      module: AppModule(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Api Marvel',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: true,
+      routerDelegate: Modular.routerDelegate,
+      routeInformationParser: Modular.routeInformationParser,
+    );
+  }
+}
